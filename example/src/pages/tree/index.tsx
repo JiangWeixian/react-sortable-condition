d7d8f974c66c1f26dde55acef1ec2efc1341c082
JiangWeixian <saber_wei@163.com>
2019-08-24 14:57:40 +0800

    example: install react-sortable tree

---
+++ example/src/pages/tree/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import SortableTree, { TreeItem } from 'react-sortable-tree';
+import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
+
+type State = {
+  treeData: TreeItem;
+};
+
+export default class Tree extends Component<{}, State> {
+  state = {
+    treeData: [{ title: 'Chicken', children: [{ title: 'Egg' }] }],
+  };
+
+  render() {
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={this.state.treeData as any}
+          onChange={treeData => this.setState({ treeData })}
+        />
+      </div>
+    );
+  }
+}
