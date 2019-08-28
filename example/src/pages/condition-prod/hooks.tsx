2a98f62341deb01dcedb1034e5b528e986f033b0
JiangWeixian <saber_wei@163.com>
2019-08-28 15:07:37 +0800

    chore: add hooks example

---
+++ example/src/pages/condition-prod/hooks.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import { SortableCondition } from 'react-sortable-condition'
+import { useTreeData } from 'react-sortable-condition/lib/sortable-condition/useTreeData'
+
+const data: any[] = [
+  {
+    type: 'and',
+    expanded: true,
+    children: [
+      {
+        type: 'and',
+        children: [{ type: 'normal', patterns: { a: 2 } }],
+      },
+      {
+        type: 'and',
+        expanded: true,
+        children: [{ type: 'normal', patterns: { a: 2 } }, { type: 'normal', patterns: { a: 2 } }],
+      },
+    ],
+  },
+]
+
+const TestPattern = ({ patterns, onChange }: { patterns?: any; onChange?: Function }) => {
+  console.log('patterns', patterns)
+  const handleClick = () => {
+    console.log('clicked')
+    if (onChange) {
+      onChange({ patterns: 2 })
+    }
+  }
+  return <span onClick={handleClick}>1</span>
+}
+
+const Condition = () => {
+  const { treeData, dispatch } = useTreeData({ initialTreeData: data })
+  return (
+    <div style={{ height: '400px' }}>
+      <SortableCondition
+        dataSource={treeData}
+        onChange={v => {
+          console.log('change', v)
+          dispatch({ type: 'RESET', payload: v })
+        }}
+        onDragState={v => console.log('drag', v)}
+        onVisible={v => console.log('visible', v)}
+        onMoveNode={v => console.log('move', v)}
+        maxDepth={3}
+      >
+        <SortableCondition.Condition
+          onAdd={() => console.log('add')}
+          onDelete={() => console.log('delelte')}
+          // addIcon={<Icon type="plus-circle" />}
+          // deleteIcon={<Icon type="close-circle" />}
+        />
+        <SortableCondition.Pattern>
+          <TestPattern />
+        </SortableCondition.Pattern>
+      </SortableCondition>
+    </div>
+  )
+}
+
+export default Condition
